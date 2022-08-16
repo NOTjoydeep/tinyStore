decabf1a83ba0d581443394c3d3973a351ab7694
Joydeep Ghosh <joydeep.agt@gmail.com>
2022-08-16 19:13:47 +0530

    tags, addorder and add product introduced

--- store/settings.py
+++ store/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'storeapp',
     'tags',
+    'store_custom',
     'likes',
 ]
 
--- storeapp/admin.py
+++ storeapp/admin.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models.aggregates import Count
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
+
 from . import models
 
 # Register your models here.
@@ -11,32 +12,61 @@ from . import models
 
 # admin.site.register(models.Product, ProductAdmin)
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    # fields = ['title', 'slug']
+    # exclude = ['promotions']
+    # readonly_fields = ['title']
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug' : ['title']
+    }
+    actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection']
+    search_fields = ['title__istartswith']
 
     def collection_title(self, product):
         return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
-        if product.inventory == 0:
-            return 'EMPTY' + ' (' + str(product.inventory) + ')'
-        elif product.inventory < 10:
-            return 'VERY LOW' + ' (' + str(product.inventory) + ')'
-        elif product.inventory < 20:
-            return 'LOW' + ' (' + str(product.inventory) + ')'
+        if product.inventory < 10:
+            return 'Low'
         else:
-            return 'OK' + ' (' + str(product.inventory) + ')'
-    
-    # ordering = ['title', 'inventory_status']
+            return 'Ok'
+
+    @admin.action(description='Clear Inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were sucessfully updated.',
+            # messages.ERROR
+        )
+
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'product_count']
+    search_fields = ['title']
 
     # @admin.display(ordering='product_count')
     # def product_count(self, collection):
@@ -88,8 +118,25 @@ class CustomerAdmin(admin.ModelAdmin):
         )
 
 
+# class OrderItemInline(admin.TabularInline):
+#     autocomplete_fields = ['product']
+#     min_num = 1
+#     max_num = 10
+#     model = models.OrderItem
+#     extra = 0
+
+class OrderItemInline(admin.StackedInline):
+    autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+    extra = 0
+
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
     list_display = ['id', 'placed_at', 'customer']
     list_per_page = 10
     ordering = ['id', 'placed_at', 'customer']
